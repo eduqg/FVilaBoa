7fd032cccc8660f71f4837428b76263e3b79c17a
Eduardo <eduardoqgomes@gmail.com>
2015-12-09 00:09:52 -0200

    Autenticacao

--- app/controllers/artigos_controller.rb
+++ app/controllers/artigos_controller.rb
@@ -1,6 +1,7 @@
 class ArtigosController < ApplicationController
 	before_action :achar_artigo, only: [:show]
-	
+	before_action :authenticate_usuario!, except: [:index, :show]
+	#se usuario quiser entrar ele nao ser redirecionado para o sign up
 	def index
 		@artigos = Artigo.all.order("created_at DESC")
 	end
