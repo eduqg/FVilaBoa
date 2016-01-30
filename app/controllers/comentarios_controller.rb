d658ab4f5463f1d7a4dc1e2dc83bf6c8151d4a06
eduardo <eduardoqgomes@gmail.com>
2016-01-30 15:11:52 -0200

    Adicionando atualizar e exluir comentario

--- app/controllers/comentarios_controller.rb
+++ app/controllers/comentarios_controller.rb
@@ -3,10 +3,36 @@ class ComentariosController < ApplicationController
 		@artigo = Artigo.find(params[:artigo_id])
 		@comentario = @artigo.comentarios.create(params[:comentario].permit(:comentario))
 
+		@comentario.usuario_id = current_usuario.id if current_usuario
+		@comentario.save
+
 		if @comentario.save
 			redirect_to artigo_path(@artigo)
 		else
 			render 'new'
+		end
+	end
+
+	def destroy
+		@artigo = Artigo.find(params[:artigo_id])
+		@comentario = @artigo.comentarios.find(params[:id])
+		@comentario.destroy
+		redirect_to artigo_path(@artigo)
+	end
+
+	def edit
+		@artigo = Artigo.find(params[:artigo_id])
+		@comentario = @artigo.comentarios.find(params[:id])
+	end
+
+	def update
+		@artigo = Artigo.find(params[:artigo_id])
+		@comentario = @artigo.comentarios.find(params[:id])
+	
+		if @comentario.update(params[:comentario].permit(:comentario))
+			redirect_to artigo_path(@artigo)
+		else
+			render 'edit'
 		end
 	end
 end
